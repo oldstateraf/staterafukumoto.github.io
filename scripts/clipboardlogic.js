571a951d5d9f3d7c35594f76dcbc7bc2b998d3a5
wayoutware sp <48106340+wayoutware@users.noreply.github.com>
2019-06-17 23:29:39 -0400

    design update 3
    improved alert banner, slick link copy effect, and custom dark scrollbars (i promise i didn't make it look fresh out of 2003)

--- scripts/clipboardlogic.js
+++ scripts/clipboardlogic.js
@@ -1,17 +1,3 @@
-// function myFunction() {
-//     /* Get the text field */
-//     var copyText = document.getElementById("myInput");
-  
-//     /* Select the text field */
-//     copyText.select();
-  
-//     /* Copy the text inside the text field */
-//     document.execCommand("copy");
-  
-//     /* Alert the copied text */
-//     alert("Copied the text: " + copyText.value);
-// }
-
 window.domain = "https://staterafukumoto.github.io"
 window.sep = "#"
 
