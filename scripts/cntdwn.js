415447d04831e9806bcecdc82416b8d38bdfd1b7
wayoutware sp <48106340+wayoutware@users.noreply.github.com>
2019-07-23 04:14:11 -0400

    updated countdown
    it's the final countdown
    DUN NUN NUU NUU

--- scripts/cntdwn.js
+++ scripts/cntdwn.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jul 23, 2019 14:00:00").getTime();
+var countDownDate = new Date("Jul 23, 2019 15:33:00").getTime();
 
 function countdown(){
     // get today's date and time
